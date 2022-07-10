5930c044de6b8c860f51877fd0fe8451ff15deba
yuanhao <huzhen@inke.cn>
2022-07-10 17:02:32 +0800

    deploy

--- src/pages/AmisPages/Page1/index.js
+++ src/pages/AmisPages/Page1/index.js
@@ -2,17 +2,10 @@ import React, { Component } from "react";
 
 import renderAmis from "src/amis";
 
+import amisJSON from "./amis.json";
+
 function Page1() {
-  return (
-    <div>
-      {renderAmis({
-        // 这里是 amis 的 Json 配置。
-        type: "page",
-        title: "简单页面",
-        body: "内容",
-      })}
-    </div>
-  );
+  return <div>{renderAmis(amisJSON)}</div>;
 }
 
 export default Page1;
